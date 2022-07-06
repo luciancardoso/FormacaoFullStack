815293afcc9a5bed6ca2abff4822bc6e5112b548
Lucian Cardoso <luciancardoso20@gmail.com>
2022-07-05 22:04:30 -0400

    Adicionando metodo GET

--- NODE/api-restful/src/controllers/products.js
+++ NODE/api-restful/src/controllers/products.js
@@ -2,7 +2,20 @@ const ProductsModel = require('../models/products')
 
 async function get(req, res){
 
-    const products = await ProductsModel.find()
+    const { id } = req.params
+
+    // refatorando o if
+    // let obj = {}
+
+    // if(id){
+    //     obj._id = id
+    // }
+
+    // if ternario aqui se eu tiver um objeto quero que atribuia a um objeto
+    // esse objeto vai ter um (_id: id) caso contrario eu quero que seja null
+    const obj = id ? { _id: id } : null
+
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
 
