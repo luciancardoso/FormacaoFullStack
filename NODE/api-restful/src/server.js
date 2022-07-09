c8c7f3d85b0ce790d315a8f55972b26cccfb9d05
Lucian Cardoso <luciancardoso20@gmail.com>
2022-07-09 15:19:15 -0400

    adicionando Middleware de CORS

--- NODE/api-restful/src/server.js
+++ NODE/api-restful/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes')
@@ -8,6 +8,34 @@ const app = express()
 
 // conexão com o bando de dados
 db.connect()
+
+// habilita CORS
+// app.use(cors())
+
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://www.app.com.br',
+]
+
+// aqui estou permitindo apenas um endereço caso eu queira permitir mais de um endereço preciso
+// preciso criar uma arrays de strings e jogar dentro da arrays os endereços que quero permitir
+// app.use(cors({
+//     origin: 'http://www.nossaaplicacao.com.br'
+// }))
+
+app.use(cors({
+    origin: function(origin, callback) {
+        let allowed = true
+
+        // mobile app
+        if(!origin) allowed = true
+
+        // se não estiver dentro 
+        if(!allowedOrigins.includes(origin)) allowed = false
+
+        callback(null, allowed)
+    }
+}))
 
 // habilita server para receber dados json
 app.use(express.json())
