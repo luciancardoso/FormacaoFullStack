3c89092bbb389712b7044964821bcb758bc0bd76
Lucian Cardoso <luciancardoso20@gmail.com>
2023-01-22 22:29:05 -0400

    Estilos Globais e configuração de Tema

--- React/my_app/src/index.js
+++ React/my_app/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { deepPurple, red } from '@material-ui/core/colors';
+
 import App from './App';
+import './index.css'
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[900],
+    },
+    secondary: {
+      main: red[900],
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
