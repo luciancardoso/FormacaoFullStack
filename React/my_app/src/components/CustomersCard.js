9ba6266596e5407f52eddcdfa9d8b845c386f343
Lucian Cardoso <luciancardoso20@gmail.com>
2023-01-29 18:45:07 -0400

    Adicionando Grid e biblioteca ClassNames

--- React/my_app/src/components/CustomersCard.js
+++ React/my_app/src/components/CustomersCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
+import {
+  Card,
+  CardHeader,
+  CardActions,
+  Avatar,
+  IconButton
+} from '@material-ui/core';
 
-import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 
@@ -17,11 +20,11 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const CustomersCard = ({ name, lastname, email, avatar }) => {
+const CustomersCard = ({ name, lastname, email, avatar, className }) => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
+    <Card className={classNames(className ,classes.root)}>
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" src={avatar}>
--- React/my_app/src/pages/Customers.js
+++ React/my_app/src/pages/Customers.js
@@ -1,12 +1,26 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import CustomersCard from '../components/CustomersCard'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    card: {
+    //   padding: theme.spacing(2),
+    margin: theme.spacing(2),
+    },
+  }));
 
 const Customers = () => {
 
+    const classes = useStyles();
+
     const [customers, setCustomers] = useState([])
 
-    console.log(customers)
+    // console.log(customers)
 
     useEffect(() => {
         axios.get('https://reqres.in/api/users')
@@ -20,16 +34,21 @@ const Customers = () => {
     return(
         <>
             <h1>Customers</h1>
-            {
-                customers.map(item => (
-                    <CustomersCard
-                        name={item.first_name}
-                        lastname={item.last_name}
-                        email={item.email}
-                        avatar={item.avatar}
-                    />
-                ))
-            }
+            <Grid container>
+                {
+                    customers.map(item => (
+                        <Grid xs={6} md={4}>
+                            <CustomersCard
+                                name={item.first_name}
+                                lastname={item.last_name}
+                                email={item.email}
+                                avatar={item.avatar}
+                                className={classes.card}
+                            />
+                        </Grid>
+                    ))
+                }
+            </Grid>
         </>
     )
 }
