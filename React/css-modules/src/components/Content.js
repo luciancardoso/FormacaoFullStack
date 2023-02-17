7024064b392597faf6f3acf199189cb95ed997db
Lucian Cardoso <luciancardoso20@gmail.com>
2023-02-17 17:33:10 -0400

    Esdudo sobre CSS-MODULE

---
+++ React/css-modules/src/components/Content.js
@@ -0,0 +1,14 @@
+import style from './Content.module.css'
+
+const Content = () => {
+    return(
+        <div className={style.Content}>
+            <h2>Título</h2>
+            <p>
+                Lorem Ipsum
+            </p>
+        </div>
+    )
+}
+
+export default Content
